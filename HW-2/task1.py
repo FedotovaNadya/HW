0c023469f0327f357988d0a358378f0929e02a9e
Nadya_Fedotova <56261645+FedotovaNadya@users.noreply.github.com>
2019-11-03 23:39:38 +0300

    Create task1.py

---
+++ HW-2/task1.py
@@ -0,0 +1,13 @@
+n=int(input("Enter days number: "))
+result = []
+for i in range(n):
+  v=[]
+  m=int(input("Enter running number: "))
+  for i in range(m):
+    TimeS=input()
+    TimeS = TimeS.split()
+    v.append(int(TimeS[1])/int(TimeS[0]))
+  result.append(sum(v))
+result.sort()
+for i in range(len(result)):
+  print(result[i])
